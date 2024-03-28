89698dbfad36b8e977809b51f601f120a677dc51
allylavanda <allylavanda@aol.com>
2024-03-27 22:25:31 -0400

    Update Main.java

--- Battleship/src/Main.java
+++ Battleship/src/Main.java
@@ -1,2 +1,11 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    public void start(Stage s){
+        s.setTitle("Battleship Game");
+    }
 }
